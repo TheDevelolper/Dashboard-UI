44269eb662a013edd605eef45b30dcc45da61762
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2026-02-26 21:10:23 +0000

    feat(storybook): add accessibility addon

--- projects/angular-workspace/.storybook/main.ts
+++ projects/angular-workspace/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../projects/component-lib/src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@chromatic-com/storybook', '@storybook/addon-docs'],
+  addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
     options: {},
